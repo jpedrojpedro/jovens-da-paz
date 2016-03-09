99a2a8cc37d677ec611a160cd46fa815a253c8b6
jpedrojpedro <joaopedro.pinheiro88@gmail.com>
2016-03-09 01:35:48 -0300

    Organize CSS and JS import order; Organize partials; Adapt template to the one suggested

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,18 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require jquery.min
+//= require bootstrap.min
+//= require ./plugins/wow.min
+//= require ./plugins/owl.carousel.min
+//= require ./plugins/jquery.parallax-1.1.3
+//= require ./plugins/jquery.magnific-popup.min
+//= require ./plugins/jquery.mb.YTPlayer.min
+//= require ./plugins/jquery.countTo
+//= require ./plugins/jquery.inview.min
+//= require ./plugins/pace.min
+//= require ./plugins/jquery.easing.min
+//= require ./plugins/jquery.validate.min
+//= require ./plugins/additional-methods.min
+//= require hallooou
+//
