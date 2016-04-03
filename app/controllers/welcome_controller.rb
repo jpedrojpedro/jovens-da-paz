e5185115118084abe26e3444adf485265564ed0c
jpedrojpedro <joaopedro.pinheiro88@gmail.com>
2016-04-03 13:42:45 -0300

    Add embed Google Form - Enrollment EJC 68

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def enrollment
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  get 'inscricao' => 'welcome#enrollment'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
